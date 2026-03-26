d390b05d736c0247f15529e743332b362008a0c5
0-don <don.cryptus@gmail.com>
2026-03-26 12:39:20 +0100

    Add clippy cask v1.5.0

---
+++ Casks/clippy.rb
@@ -0,0 +1,17 @@
+cask "clippy" do
+  version "1.5.0"
+  sha256 "PLACEHOLDER_SHA256"
+
+  url "https://github.com/0-don/clippy/releases/download/v#{version}/clippy_#{version}_aarch64.dmg"
+  name "Clippy"
+  desc "Clipboard Manager built with Rust & Typescript"
+  homepage "https://github.com/0-don/clippy"
+
+  app "clippy.app"
+
+  zap trash: [
+    "~/Library/Application Support/clippy",
+    "~/Library/Caches/clippy",
+    "~/Library/Preferences/clippy.plist",
+  ]
+end
