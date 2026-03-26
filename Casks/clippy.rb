c1d0afbdea7ccb557bc93910632960d95ccea169
0-don <0-don@users.noreply.github.com>
2026-03-26 14:23:33 +0000

    Update to 1.5.3

--- Casks/clippy.rb
+++ Casks/clippy.rb
@@ -1,10 +1,10 @@
 cask "clippy" do
-  version "1.5.0"
-  sha256 "0a704064031f52525f8f928bd9eb3143ab89e0efbf79c7604c971159b8cdf2fb"
+  version "1.5.3"
+  sha256 "4ae416a46671c06ec5882120c89cb455af8647517e52d1353e1b58d2f386c282"
 
   url "https://github.com/0-don/clippy/releases/download/v#{version}/clippy_#{version}_aarch64.dmg"
   name "Clippy"
-  desc "Clipboard Manager built with Rust & Typescript"
+  desc "Clipboard Manager built with Rust {{DESCRIPTION}} Typescript"
   homepage "https://github.com/0-don/clippy"
 
   app "clippy.app"
