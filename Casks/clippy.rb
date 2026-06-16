55ffb6d724736265e317e17487791e1d9b89efac
0-don <0-don@users.noreply.github.com>
2026-06-16 19:30:55 +0000

    Update to 1.6.19

--- Casks/clippy.rb
+++ Casks/clippy.rb
@@ -1,6 +1,6 @@
 cask "clippy" do
-  version "1.6.18"
-  sha256 "8d07109dc8107af08bffc9d328dffd552254b941cd902f9b6dfa88c82b3b0def"
+  version "1.6.19"
+  sha256 "06c4c3a0d171dc679d2d22ecc68c7449c5d925b815bef7d86940866842008ce3"
 
   url "https://github.com/0-don/clippy/releases/download/v#{version}/clippy_#{version}_universal.dmg"
   name "Clippy"
